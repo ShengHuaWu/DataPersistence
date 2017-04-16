e83594b3fec653bddd6de678c04cb1b7df28c1c8
ShengHua Wu <fantasy0404@gmail.com>
2017-04-16 17:46:49 +0800

    Write tests for Database.

---
+++ DataPersistenceTests/DatabaseTests.swift
@@ -0,0 +1,87 @@
+//
+//  DatabaseTests.swift
+//  DataPersistence
+//
+//  Created by ShengHua Wu on 16/04/2017.
+//  Copyright © 2017 ShengHua Wu. All rights reserved.
+//
+
+import XCTest
+import RealmSwift
+@testable import DataPersistence
+
+class DatabaseTests: XCTestCase {
+    // MARK: - Properties
+    var database: Database!
+    
+    // MARK: - Overrider Methods
+    override func setUp() {
+        super.setUp()
+        
+        let realm = try! Realm(configuration: Realm.Configuration(inMemoryIdentifier: "com.shenghuawu.DataPersistence"))
+        database = Database(realm: realm)
+    }
+    
+    override func tearDown() {
+        database.deleteAll()
+        database = nil
+        
+        super.tearDown()
+    }
+    
+    // MARK: - Enabled Tests
+    func testBookCreation() {
+        let newBook = Book.bigLittleLies
+        
+        database.createOrUpdate(model: newBook, with: Book.createOrUpdate)
+        
+        database.verifyBookCreationOrUpdating(newBook: newBook)
+    }
+    
+    func testBookDeletion() {
+        let newBook = Book.southAndWest
+        database.createOrUpdate(model: newBook, with: Book.createOrUpdate)
+        let bookInDB = database.fetch(with: Book.all).first!
+        
+        database.delete(with: bookInDB.delete)
+        
+        database.verifyBookDeletion()
+    }
+    
+    func testDrinkUpdating() {
+        let newBook = Book.fake
+        database.createOrUpdate(model: newBook, with: Book.createOrUpdate)
+        let bookInDB = database.fetch(with: Book.all).first!
+        
+        let modifiedBook = Book(bookID: bookInDB.bookID, name: "New Name", comment: "Change the rating and the name of this book.", rating: .mediocre)
+        database.createOrUpdate(model: modifiedBook, with: Book.createOrUpdate)
+        
+        database.verifyBookCreationOrUpdating(newBook: modifiedBook)
+    }
+}
+
+// MARK: - Verify
+extension Database {
+    func verifyBookCreationOrUpdating(newBook: Book, file: StaticString = #file, line: UInt = #line) {
+        let results = fetch(with: Book.all)
+        XCTAssertEqual(results.count, 1, "results count", file: file, line: line)
+        
+        let bookInDB = results.first!
+        XCTAssert(bookInDB.bookID.characters.count > 0, "book ID is empty", file: file, line: line)
+        XCTAssertEqual(bookInDB.name, newBook.name, "name", file: file, line: line)
+        XCTAssertEqual(bookInDB.comment, newBook.comment, "comment", file: file, line: line)
+        XCTAssertEqual(bookInDB.rating, newBook.rating, "rating", file: file, line: line)
+    }
+    
+    func verifyBookDeletion(file: StaticString = #file, line: UInt = #line) {
+        let results = fetch(with: Book.all)
+        XCTAssertEqual(results.count, 0, "results count", file: file, line: line)
+    }
+}
+
+// MARK: - Seed Data
+extension Book {
+    static let bigLittleLies = Book(bookID: "", name: "Big Little Lies", comment: "Awesome!!", rating: .outstanding)
+    static let southAndWest = Book(bookID: "", name: "South and West", comment: nil, rating: .good)
+    static let fake = Book(bookID: "", name: "Fake Book", comment: "This is a bad example.", rating: .notRecommended)
+}
