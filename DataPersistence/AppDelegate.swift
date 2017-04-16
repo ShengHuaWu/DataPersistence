1f8e358d0d6610290b4ae3b75fc5fe3cebb2b037
ShengHua Wu <fantasy0404@gmail.com>
2017-04-16 16:50:42 +0800

    First commit.

---
+++ DataPersistence/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  DataPersistence
+//
+//  Created by ShengHua Wu on 16/04/2017.
+//  Copyright © 2017 ShengHua Wu. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
